206ae8a5515ab351bb2b64680af596c8fe21d0e4
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-02-19 19:57:39 +0800

    修改更新

--- src/main/java/ne/fnfal113/fnamplifications/FNAmplifications.java
+++ src/main/java/ne/fnfal113/fnamplifications/FNAmplifications.java
@@ -59,8 +59,9 @@ public final class FNAmplifications extends JavaPlugin implements SlimefunAddon 
         getConfig().options().copyDefaults();
         saveDefaultConfig();
 
-        if (getConfig().getBoolean("auto-update", true) && getDescription().getVersion().startsWith("DEV - ")) {
-            new GitHubBuildsUpdater(this, getFile(), "buiawpkgew1/FN-FAL-s-Amplifications-zh/main").start();
+        if (getConfig().getBoolean("auto-update") &&
+            getDescription().getVersion().startsWith("Build")) {
+            new GuizhanBuildsUpdater(this, getFile(), "buiawpkgew1", "FN-FAL-s-Amplifications-zh", "main", false).start();
         }
     }
 
