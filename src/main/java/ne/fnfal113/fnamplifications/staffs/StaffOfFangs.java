8aa1a6770bf74dacbff44f0d237af95bbf235d0a
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-04-14 17:28:04 +0800

    Update StaffOfFangs.java

--- src/main/java/ne/fnfal113/fnamplifications/staffs/StaffOfFangs.java
+++ src/main/java/ne/fnfal113/fnamplifications/staffs/StaffOfFangs.java
@@ -52,7 +52,7 @@ public class StaffOfFangs extends AbstractStaff {
                 block,
                 Interaction.BREAK_BLOCK)
         ) {
-            player.sendMessage(ChatColor.DARK_RED + "You don't have permission to cast fangs there!");
+            player.sendMessage(ChatColor.DARK_RED + "你没有允许在那里铸造f牙!");
             return;
         }
 
