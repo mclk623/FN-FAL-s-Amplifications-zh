4a7fab1ef346d5d157a708e88be56e86eb080653
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-04-14 17:27:50 +0800

    Update StaffOfExplosion.java

--- src/main/java/ne/fnfal113/fnamplifications/staffs/StaffOfExplosion.java
+++ src/main/java/ne/fnfal113/fnamplifications/staffs/StaffOfExplosion.java
@@ -51,7 +51,7 @@ public class StaffOfExplosion extends AbstractStaff {
                 block,
                 Interaction.BREAK_BLOCK)
         ) {
-            player.sendMessage(ChatColor.DARK_RED + "You don't have permission to cast explosion there!");
+            player.sendMessage(ChatColor.DARK_RED + "你没有允许在那里施加爆炸!");
             return;
         }
 
