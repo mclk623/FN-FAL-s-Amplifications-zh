399623678081c2fb35722c0a4d53a7ecf9cf03bb
mclk623 <mclk623@163.com>
2026-02-08 22:07:44 +0800

    Fix jukebox https://github.com/SlimefunGuguProject/FN-FAL-s-Amplifications/pull/11/changes/f6a1bc30f078e7124b4dc21524dcd573bf06f382

--- src/main/java/ne/fnfal113/fnamplifications/machines/listener/JukeBoxClickListener.java
+++ src/main/java/ne/fnfal113/fnamplifications/machines/listener/JukeBoxClickListener.java
@@ -1,34 +1,39 @@
 package ne.fnfal113.fnamplifications.machines.listener;
 
 import ne.fnfal113.fnamplifications.utils.Utils;
-import ne.fnfal113.fnamplifications.utils.compatibility.VersionedClass;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import org.bukkit.Material;
+import org.bukkit.Tag;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
+import javax.annotation.Nonnull;
+import java.util.Locale;
+
 public class JukeBoxClickListener implements Listener {
 
     @EventHandler
-    public void onDiscClick(InventoryClickEvent event) {
-        String title = VersionedClass.invoke(event.getView(), "getTitle").toString();
-
-        if(title.contains(Utils.colorTranslator("&5F&dN &f唱&b片&e机"))) {
+    public void onDiscClick(InventoryClickEvent event){
+        if(event.getView().getTitle().contains(Utils.colorTranslator("&5F&dN &f唱&b片&e机"))){
             if(event.getCurrentItem() != null) {
                 ItemStack itemStack = event.getCurrentItem();
-
-                if(!itemStack.getType().name().startsWith("MUSIC_DISC") || itemStack.getItemMeta().hasEnchant(Enchantment.BINDING_CURSE) || itemStack.getType() == Material.PINK_STAINED_GLASS_PANE) {
+                if (isDisc(itemStack) && itemStack.getItemMeta().hasEnchant(Enchantment.BINDING_CURSE)) {
+                    event.setCancelled(true);
+                }
+                if(itemStack.getType() == Material.PINK_STAINED_GLASS_PANE){
+                    event.setCancelled(true);
+                }
+                if(!isDisc(itemStack)){
                     event.setCancelled(true);
                 }
             }
         }
-      
+
     }
 
+    boolean isDisc(@Nonnull ItemStack itemStack) {
+        return itemStack.getType().name().toLowerCase(Locale.ROOT).startsWith("music_disc_");
+    }
 }
